563410b3d155288552efd58c886a1fe23ec06400
trbromley <brotho02@gmail.com>
2020-03-26 14:56:31 -0400

    Maybe this is working?

--- pennylane/plugins/lightning_qubit_ops.cpp
+++ pennylane/plugins/lightning_qubit_ops.cpp
@@ -1,15 +1,8 @@
-#include <iostream>
-//#include <Eigen/Dense>
-#include <xtensor/xarray.hpp>
 #include "xtensor-blas/xlinalg.hpp"
-//using Eigen::MatrixXd;
+
 int main()
 {
-//  MatrixXd m(2,2);
-//  m(0,0) = 3;
-//  m(1,0) = 2.5;
-//  m(0,1) = -1;
-//  m(1,1) = m(1,0) + m(0,1);
-//  std::cout << m << std::endl;
-    return 0;
+    xt::xarray<double> a = {{1, 0}, {0, -1}};
+    auto d = xt::linalg::det(a);
+    std::cout << d << std::endl;  // 6.661338e-16
 }
