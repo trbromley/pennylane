70903accb53140e5b1271366c9d117bcc1bf7afb
trbromley <brotho02@gmail.com>
2020-03-27 07:59:36 -0400

    Tidy up

--- pennylane/plugins/lightning_qubit_ops.cpp
+++ pennylane/plugins/lightning_qubit_ops.cpp
@@ -9,8 +9,8 @@
 #include "xtensor/xadapt.hpp"
 #include "pybind11/stl.h"
 
-xt::pyarray<double> mvp(xt::pyarray<double> op, xt::pyarray<double> state, std::vector<unsigned
-long int> op_wires)
+xt::pyarray<double> mvp(xt::pyarray<double>& op, xt::pyarray<double>& state, std::vector<unsigned
+long int>& op_wires)
 {
     auto shape = op.shape();
     unsigned long int length = shape.size() / 2;
@@ -22,17 +22,9 @@ long int> op_wires)
     return result;
 }
 
-double sum_of_sines(xt::pyarray<double>& m)
-{
-    auto sines = xt::sin(m);  // sines does not actually hold values.
-    return std::accumulate(sines.begin(), sines.end(), 0.0);
-}
-
 PYBIND11_MODULE(lightning_qubit_ops, m)
 {
     xt::import_numpy();
     m.doc() = "Lightning qubit operations using XTensor";
-
-    m.def("sum_of_sines", sum_of_sines, "Sum the sines of the input values");
     m.def("mvp", mvp, "Matrix vector product");
 }
