fcdc20edfe19a552bb5420205ae2136ba3136322
trbromley <brotho02@gmail.com>
2020-04-14 07:19:04 -0400

    Expose order

--- pennylane/qnodes/jacobian.py
+++ pennylane/qnodes/jacobian.py
@@ -44,6 +44,7 @@ class JacobianQNode(BaseQNode):
 
         default_step_size = DEFAULT_STEP_SIZE_ANALYTIC if analytic else DEFAULT_STEP_SIZE
         self._h = kwargs.get("h", default_step_size)
+        self._order = kwargs.get("order", 1)
         """float: step size for the finite difference method"""
 
     metric_tensor = None
@@ -197,6 +198,8 @@ class JacobianQNode(BaseQNode):
         # Add the step size into the options, if it was not there already
         if "h" not in options.keys():
             options = {"h": self.h, **options}
+        if "order" not in options.keys():
+            options = {"order": self._order, **options}
 
         # (re-)construct the circuit if necessary
         if self.circuit is None or self.mutable:
@@ -318,12 +321,12 @@ class JacobianQNode(BaseQNode):
         Returns:
             array[float]: partial derivative of the node
         """
-        y0 = options.get("y0", None)
         h = options.get("h", self.h)
         order = options.get("order", 1)
 
         shift_args = args.copy()
         if order == 1:
+            y0 = options.get("y0", None)
             # shift the parameter by h
             shift_args[idx] += h
             y = np.asarray(self.evaluate(shift_args, kwargs))
